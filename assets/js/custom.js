6afbf82e1e7065f1f88c280fca29dfa3f5799214
Ronak <ronakdesai159@gmail.com>
2021-09-13 12:41:12 +0530

    1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,13 +6,7 @@ jQuery( document ).ready(function( $ ) {
 
         // Page loading animation
 
-        $("#preloader").animate({
-            'opacity': '0'
-        }, 600, function(){
-            setTimeout(function(){
-                $("#preloader").css("visibility", "hidden").fadeOut();
-            }, 300);
-        });
+        
         
 
         $(window).scroll(function() {
