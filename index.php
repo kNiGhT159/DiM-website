490d498ec9c1d4db57ecb73e8e1c96f511898178
Ronak <ronakdesai159@gmail.com>
2021-09-13 12:59:20 +0530

    1

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-    include_once('assets');
-    include_once('vendor');
     include_once('prepros-6.config');
     include_once('index.html');
-    include_once('about.html');
-    include_once('blog.html');
-    include_once('contact.html');
-    include_once('post-details.html');
 ?>
